215114a4241c40b4daab9a6aba5a654b7cea91b0
Daniel Renninghoff <daniel@danielrenninghoff.com>
2017-09-26 21:44:04 +0200

    updated with sample projects

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
   <section class="section">
     <div class="container content">
       <h3>Our Scholarship Offer</h3>
-      <p>Individuals get educated and engaged in a way that helps the country and the community. Thus, we offer financial support throughout your time at university for all living expenses, help and guidance for your social project and extra-curricular education on peacebuilding and reconciliation. The amount of the scholarship depends on the region where you live and ranges from 50-70€. You will also receive a certificate from us after successfully taking part in the scholarship program.</p>
+      <p>Individuals get educated and engaged in a way that helps the country and the community. Thus, we offer financial support throughout your time at university for all living expenses, help and guidance for your social project and extra-curricular education on peacebuilding and reconciliation. The amount of the scholarship depends on the region where you live and ranges from 50 to 70€ (approx. 9120 to 12769 SLRs) . You will also receive a certificate from us after successfully taking part in the scholarship program.</p>
 
       <h3>Application Deadline</h3>
       <p>The application deadline is October 1st, 2017.</p>
@@ -14,6 +14,19 @@
       <h3>Criteria</h3>
       <p>There are several criteria for the evaluation of the application. Eligible to apply are those who are financially in need. This time, we will only accept female applicants. This is because right now we already support two male students and we want to support both genders equally. As it is our vision to obtain a sustainable effect, you will need to develop a personal project. We would like to encourage you to develop your own original idea to improve the social conditions in your area. The project itself needs to be realistic and achievable. This will be planned and/or started during your studies and implemented after your graduation.</p>
       <p>Please describe shortly your idea for a personal project which would help to develop your region or society. If you plan to carry out the project in cooperation with a partner (whether it is another scholarship holder, fellow student or NGO) please state their name and their specific roles.</p>
+
+      <h3>About your social project</h3>
+      <p>Within the scope of the scholarship program, you will realize your own social project. To give you an idea what a social project at SOG can be about we have collected some projects of other scholarship holders in other countries:</p>
+      <ul>
+        <li>Fardina from Afghanistan teaches children in English language. Furthermore she organizes a reading and discussion club for girls where they read newspapers and discuss current happenings.</li>
+        <li>Denise from Mweso, Congo, farms her own field of tomatoes and shares her expert knowledge to community members by showing people how to cultivate.</li>
+        <li>Zalina from Chechnya founded an online language teaching platform for young people in her home country Chechnya. Her idea is that German students teach Chechen teenagers online.</li>
+        <li>Nestor from Kindu, Congo, teaches kids how to produce bricks for building schools.</li>
+      </ul>
+
+      <p>We want to encourage you to find your individual idea for a social project as you know best how to help your society and country. Your project should have an impact on society, reach as many people as possible and spread knowledge.</p>
+
+      <p>If you have any questions about the project or if you need assistance, to create your own idea, contact us under <a href="mailto:srilanka-action-scholarship@s-o-g.org">srilanka-action-scholarship@s-o-g.org</a>. Please bear in mind that SOG can't give you financial support for your project, so one of your tasks is to find possible partners (like other NGOs or governmental institutions).</p>
 
       <h3>Application</h3>
       <p>Do you think you are a suitable candidate for a scholarship? We are happy to receive your application.</p><br>
